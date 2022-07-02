72e1f390ec00d59364ccbf2d154d498b2d7ddd85
Choppa <denis.afinogenow@yandex.ru>
2022-07-02 18:02:47 +0300

    first commit

---
+++ Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿int Num(int a)
+{
+    int b = 0;
+    while(a > 0)
+    {
+        b = a % 10 + b;
+        a = a / 10;
+    }
+    return b;
+}
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Сумма числе равна " + Num(a));
---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿void Elements()
+{
+    int i = 1;
+    int a = 0;
+    int array = Convert.ToInt32(8);
+    Console.Write("[");
+    while (i < array)
+    {
+        a = new Random().Next(-1000, 1000);
+        Console.Write(a + ", ");
+        i++;
+    }
+    Console.Write(a + "]");
+}
+Elements();
