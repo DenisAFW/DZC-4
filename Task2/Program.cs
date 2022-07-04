f106e1ef2e61319f0c3262d79a93c4c703e908b0
Choppa <denis.afinogenow@yandex.ru>
2022-07-04 21:28:03 +0300

    Переделал с помощью товарищей

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,12 +1,17 @@
-﻿void Elements()
+﻿int[] Elements()
 {
     int c = 8;
     int[] array = new int[c];
     for(int i = 0; i < c; i++)
     {
         array[i] = new Random().Next(-1000, 1000);
-        Console.Write(array[i] + " ");
     }
+    return array;
 }
-Elements();
-// Не смог додумать два метода, не смог привести один метод(Elements), на метод вывода.
+
+void Print(int [] mass)
+{
+    String mis = string.Join(", ", mass);
+    Console.WriteLine("[" + mis + "]");
+}
+Print(Elements());
